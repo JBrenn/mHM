13aa26b7897c52e6acf15b0a5715a666dc6d4aa3
JBrenn <jobrenne@outlook.com>
2016-11-23 20:53:30 +0100

    added man, improved doc

--- R/mHM_maskRaster.R
+++ R/mHM_maskRaster.R
@@ -1,3 +1,20 @@
+#' Mask raster maps with same projection and extent. 
+#' 
+#' \code{mHM_maskRaster} masks a raster with same projection and extent as the provided mask.
+#' 
+#' 
+#' 
+#' @param mask raster, either ASCII or GEOtif format.
+#' @param inRaster character vector, gives location and format of raster files (either ASCII or GEOtif), 
+#' with same projection and extent as provided raster mask.
+#' @param outRaster character vector, defining raster format and location of the outfile, same length as \code{inRaster}.
+#' 
+#' @return raster in given format (\code{outRaster}) written to outRaster
+#' @examples
+#' 
+#' @export mHM_maskRaster
+#' 
+
 # mask raster map same proj and extent
 # library(raster)
 
@@ -5,30 +22,20 @@ mHM_maskRaster <- function(mask, inRaster, outRaster)
 {
   # read in mask
   maskR <- raster(mask)
+  # locations of invalid values in raster mask 
   na_mask <- which(is.na(values(maskR)))
   
-  # read in raster files2mask
+  # read in raster files
+  # loop over inRaster
   for (i in 1:length(inRaster))
   {
+    # print raster map that will be masked
     print(i)
+    # load raster
     inR <-  raster(inRaster[i])
+    # mask raster
     inR[na_mask] <- NA
+    # write out raster
     writeRaster(x = inR, filename = outRaster[i], overwrite=T)
-  }
-}
-
-mHM_formatASC <- function(inRaster)
-{
-  for (write2 in inRaster)
-  {
-    # correct header format
-    system(paste("sed -i -e 's/.000000000000000 / /g' ", write2, sep=""))
-    system(paste("sed -i -e 's/NCOLS/ncols/g' ", write2, sep=""))
-    system(paste("sed -i -e 's/NROWS/nrows/g' ", write2, sep=""))
-    system(paste("sed -i -e 's/NCOLS/ncols/g' ", write2, sep=""))
-    system(paste("sed -i -e 's/XLLCORNER/xllcorner/g' ", write2, sep=""))
-    system(paste("sed -i -e 's/YLLCORNER/yllcorner/g' ", write2, sep=""))
-    system(paste("sed -i -e 's/CELLSIZE/cellsize/g' ", write2, sep=""))
-    system(paste("rm ", write2, "-e", sep=""))
   }
 }
