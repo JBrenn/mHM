bdfb476ffef706ca28f33c599a73e899861103ca
JBrenn <jobrenne@outlook.com>
2016-11-24 11:22:45 +0100

    added working example, loop over ext - data get updated now, radius of aggregation for each iteration: 1 grid cell

--- R/mHM_extrapolMat.R
+++ R/mHM_extrapolMat.R
@@ -14,6 +14,20 @@
 #' @return matrix.
 #' 
 #' @examples
+#' # read netcdf file minimum air temperature
+#' pkg_path <- path.package("mHMr")
+#' tmin_netcdf <- file.path(pkg_path, "data", "tmin.nc")
+#' tmin_op <-  RNetCDF::open.nc(tmin_netcdf)
+#' tmin <- RNetCDF::var.get.nc(tmin_op,"tmin")
+#' image(tmin[,,1])
+#' 
+#' tmin1 <- mHM_extrapolMat(tmin[,,1], ext=1)
+#' image(tmin1,  zlim = c(2.5,4.5))
+#' 
+#' tmin2 <- mHM_extrapolMat(tmin[,,1], ext=2)
+#' image(tmin2,  zlim = c(2.5,4.5))
+#' 
+#' 
 #' 
 #' @author Johannes Brenner \email{johannes.brenner@ufz.de}
 #' 
@@ -28,36 +42,36 @@
 
 mHM_extrapolMat <- function(data, ext)
 {
-  data <- data; ext <- ext
-  
-  # get invalid locations in array
-  arrTind <- which(is.na(data), arr.ind = T)
-  # apply function over these locations by row
-  corr <- apply(X = arrTind, MARGIN = 1, function(x, data, ext) {
-    # define rows and colums to use for aggregation
-    rowsl <- x[1]-ext; rowsu <- x[1]+ext
-    if (rowsl < 1) rowsl <- 1
-    if (rowsu > dim(data)[1]) rowsu <- dim(data)[1]
-    rows <- (rowsl):(rowsu)
+  for (i in 1:ext)
+  {
+    # get invalid locations in array
+    arrTind <- which(is.na(data), arr.ind = T)
+    # apply function over these locations by row
+    corr <- apply(X = arrTind, MARGIN = 1, function(x, data) {
+      # define rows and colums to use for aggregation
+      rowsl <- x[1]-1; rowsu <- x[1]+1
+      if (rowsl < 1) rowsl <- 1
+      if (rowsu > dim(data)[1]) rowsu <- dim(data)[1]
+      rows <- (rowsl):(rowsu)
+      
+      colsl <- x[2]-1; colsu <- x[2]+1
+      if (colsl < 1) colsl <- 1
+      if (colsu > dim(data)[2]) colsu <- dim(data)[2]
+      cols <- (colsl):(colsu)
+      
+      # if there are only NAs within choosen values set new value to NA
+      # otherwise take mean value
+      if (any(!is.na(data[rows,cols]))) {
+        y <- mean(data[rows,cols], na.rm=T)
+      } else {
+        y <- NA
+      }
+      return(y)
+    }, data=data)
     
-    colsl <- x[2]-ext; colsu <- x[2]+ext
-    if (colsl < 1) colsl <- 1
-    if (colsu > dim(data)[2]) colsu <- dim(data)[2]
-    cols <- (colsl):(colsu)
-    
-    # if there are only NAs within choosen values set new value to NA
-    # otherwise take mean value
-    if (any(!is.na(data[rows,cols]))) {
-      y <- mean(data[rows,cols], na.rm=T)
-    } else {
-      y <- NA
-    }
-    return(y)
-  }, data=data, ext=ext)
-  
-  # impute new values in data matrix
-  for (i in 1:dim(arrTind)[1]) data[arrTind[i,1],arrTind[i,2]] <- corr[i]
-  
+    # update values in data matrix
+    for (i in 1:dim(arrTind)[1]) data[arrTind[i,1],arrTind[i,2]] <- corr[i]
+  }
   # give back matrix
   return(data)
 }
