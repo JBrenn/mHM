9a8a0d1adcd64e9c95bb7d697deedec08b3bc6bc
Johannes Brenner brennerj <brennerj@ces251m.intranet.ufz.de>
2017-07-28 21:04:57 +0200

    inFile option implemented for reading mHM discharge constain data

--- R/mHM_readQ.R
+++ R/mHM_readQ.R
@@ -1,31 +1,44 @@
-#' Read mHM daily_discharge.out in zoo object.
-#' 
-#' @param outQpath path to folder containing the file daily_discharge.
+#' Read mHM discharge data in zoo object.
+#'
+#' @param Qpath path to folder containing the file daily_discharge (mHM constrain or output).
 #' @param dischargeFile name of discharge file, default = "daily_discharge.out"
-#' 
+#' @param inFile boolean, is given discharge file in mHM constraining data, default = FALSE
+#'
 #' @return daily zoo object including observed and simulated discharge.
-#' 
+#'
 #' @examples
-#' outQ <- mHM_readQ(outQpath="your/path")
-#' 
+#' outQ <- mHM_readQ(Qpath="your/path")
+#'
 #' @author Johannes Brenner \email{johannes.brenner@ufz.de}
-#' 
+#'
 #' @references
-#' 
+#'
 #' @seealso
-#' 
+#'
 #' @keywords
-#' 
+#'
 #' @export mHM_readQ
 
-mHM_readQ <- function(outQpath, dischargeFile = "daily_discharge.out")
+mHM_readQ <- function(Qpath, dischargeFile = "daily_discharge.out", inFile=FALSE)
 {
-  # read daily_discharge.out file
-  qout <- readr::read_table(file = file.path(outQpath, dischargeFile), na = c("NA", "-9999"))
-  
-  # make time series / zoo object
-  days <- as.Date(paste(qout$Year, qout$Mon, qout$Day, sep="-"), format = "%Y-%m-%d")
-  qout_zoo <- zoo::zoo(qout[,c(5,6)], days)
-  
+  if (inFile) {
+    # read daily_discharge.in file
+    qin  <- readr::read_table(file = file.path(Qpath, dischargeFile),
+                              na = c("NA", "-9999", "-9999.000", "999.000"),
+                              skip = 5, col_names = F)
+    # make time series / zoo object
+    days <- as.Date(paste(qin$X1, qin$X2, qin$X3, sep="-"), format = "%Y-%m-%d")
+    qout_zoo <- zoo::zoo(qin$X6, days)
+  } else {
+    # read daily_discharge.out file
+    qout <- readr::read_table(file = file.path(Qpath, dischargeFile),
+                              na = c("NA", "-9999"))
+
+    # make time series / zoo object
+    days <- as.Date(paste(qout$Year, qout$Mon, qout$Day, sep="-"), format = "%Y-%m-%d")
+    qout_zoo <- zoo::zoo(qout[,c(5,6)], days)
+  }
+
+
   return(qout_zoo)
 }
