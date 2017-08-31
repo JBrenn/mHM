73b860d78c2c5092b17bce507beb3347fdd6e53d
Johannes Brenner brennerj <brennerj@ces251m.intranet.ufz.de>
2017-08-31 12:11:58 +0200

    new arg return, extended man

--- R/mHM_extrapolNetCDF.R
+++ R/mHM_extrapolNetCDF.R
@@ -5,26 +5,35 @@
 #' 
 #' Runing mHM the first time for some netCDF products (e.g. Spain02, E-OBS) the warning
 #' no meteorological input available for whole simulation domain will be printed. 
-#' This function extrapolates grids in invalid locations within a radius given by \code{ext}.
+#' This function extrapolates grids in invalid locations within a radius (number of grid cells) given by \code{ext}.
 #' Extrapolation is performed by a mean value of the surrounding cells of the invalid location.
 #' 
-#' @param netCDF netCDF file name, full path.
-#' @param var name of variable to extrapolate.
-#' @param ext radius within grid cells are extended, starting from valid values.
+#' Caution: netCDF input file will be overwritten, backup original file before use.
 #' 
-#' @return netCDF file.
+#' @param netCDF netCDF file name, full path.
+#' @param var character, name of variable to extrapolate.
+#' @param ext integer, radius (number of grid cells) within grid cells are extended, starting from border grid cells.
+#' @param return boolean, should extrapolated array be returned in console? default=FALSE
+#' 
+#' @return netCDF file 
 #' 
 #' @examples
-#' # read netcdf file minimum air temperature
-#' pkg_path <- path.package("mHMr")
-#' tmin_netcdf <- file.path(pkg_path, "data", "tmin.nc")
-#' 
-#' tmin_op <-  RNetCDF::open.nc(tmin_netcdf)
-#' tmin <- RNetCDF::var.get.nc(tmin_op,"tmin")
-#' image(tmin[,,1], zlim = c(2.5,4.5))
-#' 
-#' tmin_ext <- mHM_tmin_netcdf(netCDF = tmin_netcdf, vat = "tmin", ext = 1)
-#' image(tmin_ext[,,1], zlim = c(2.5,4.5))
+#'# read netcdf file minimum air temperature for Júcar domain
+#'  pkg_path <- path.package("mHMr")
+#'  tmin_netcdf <- file.path(pkg_path, "data", "tmin.nc")
+#'
+#'# open connection to netCDF
+#'  tmin_op <-  RNetCDF::open.nc(tmin_netcdf)
+#'# retrieve min. air temp  
+#'  tmin <- RNetCDF::var.get.nc(tmin_op,"tmin")
+#'# visualise original field
+#'  image(tmin[,,1], zlim = c(2.5,4.5))
+#'
+#'# extrapolate (radius: 1 grid cell)
+#'  tmin_ext <- mHM_tmin_netcdf(netCDF = tmin_netcdf, vat = "tmin", ext = 1)
+#'# visualise extrapolated field
+#'  image(tmin_ext[,,1], zlim = c(2.5,4.5))
+#'
 #' 
 #' @author Johannes Brenner \email{johannes.brenner@ufz.de}
 #' 
@@ -37,7 +46,7 @@
 #' @export mHM_extrapolNetCDF
 #' 
 
-mHM_extrapolNetCDF <- function(netCDF, var, ext)
+mHM_extrapolNetCDF <- function(netCDF, var, ext, return=FALSE)
 {
   # open connection to netCDF file
   nccon <- RNetCDF::open.nc(netCDF, write = TRUE)
@@ -58,7 +67,7 @@ mHM_extrapolNetCDF <- function(netCDF, var, ext)
   RNetCDF::close.nc(nccon)
   
   # return matrix
-  return(ncvar)
+  if (return) return(ncvar) else return(NULL)
 }
 
 
